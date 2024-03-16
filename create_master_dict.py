76c7e39c1cfa2e159b68922805b0db8caafd3fdb
ege <ege.erdem@gmail.com>
2024-03-16 17:37:09 +0100

    figures

--- create_master_dict.py
+++ create_master_dict.py
@@ -1,10 +1,17 @@
 
-### Directories for results and ground truth
+### Directories for estimations and ground truth
 
-# Junyan Jiang1,2,3 Ke Chen1,2 Wei Li1 Gus Xia (2019). Large-Vocabulary Chord Transcription
-# via Chord Structure Decomposition. 20th International Society for (ISMIR 2019)
+# Large-Vocabulary Chord Transcription via Chord Structure Decomposition (ISMIR 2019)
+# file naming: rwc-pop_10.lab
 model1_dir = "./ISMIR_largevocab_results"
 
+# A Bi-directional Transformer for Musical Chord Recognition (ISMIR 2019)
+# file naming: rwc-pop_8.flac.lab
+model2_dir = "./transformer_results"
+
+# AUTOCHORD: AUTOMATIC CHORD RECOGNITION LIBRARY AND CHORD VISUALIZATION APP (ISMIR 2021)
+# file naming: rwc-pop_8.flac.lab
+model3_dir = "./autochord_results"
 
 true_dir = "./rwc-pop-annot"  # RWC Pop Dataset
 
@@ -14,21 +21,58 @@ import mir_eval
 
 # Iterate over files in the directory
 
-dict = {}
+#model1 : ISMIR_largevocab
+model1_dict = {}
 for filename in os.listdir(model1_dir):
     # Check if the file ends with ".lab"
     if filename.endswith(".lab"):
         # Extract the number between underscore and ".lab" using regular expression
+        # file naming: rwc-pop_10.lab
         match = re.search(r'_([0-9]+)\.lab$', filename)
         model1_id = int(match.group(1))  # Convert the matched string to integer
         print(f"File: {filename}, ID: {model1_id}")
-        dict[model1_id] = filename
+        model1_dict[model1_id] = filename
 
-sorted_keys = sorted(dict.keys())
-model1_dict = {key: dict[key] for key in sorted_keys}
+sorted_keys = sorted(model1_dict.keys())
+model1_dict_names = {key: model1_dict[key] for key in sorted_keys}
+
+#model2 : Transformer
+model2_dict = {}
+for filename in os.listdir(model2_dir):
+    # file naming: rwc-pop_8.flac.lab
+    # Check if the file ends with ".lab"
+    if filename.endswith(".lab"):
+        # Extract the number between underscore and ".lab" using regular expression
+        # file naming: rwc-pop_10.lab
+        match = re.search(r'_([0-9]+)\.flac$', filename)
+        model2_id = int(match.group(1))  # Convert the matched string to integer
+        print(f"File: {filename}, ID: {model2_id}")
+        model2_dict[model2_id] = filename
+
+sorted_keys = sorted(model2_dict.keys())
+model2_dict_names = {key: model2_dict[key] for key in sorted_keys}
+
+#model3 : Autochord
+model3_dict = {}
+for filename in os.listdir(model3_dir):
+    # file naming: rwc-pop_8.flac.lab
+    # Check if the file ends with ".lab"
+    if filename.endswith(".lab"):
+        # Extract the number between underscore and ".lab" using regular expression
+        # file naming: rwc-pop_10.flac.lab
+        match = re.search(r'_([0-9]+)\.flac$', filename)
+        model3_id = int(match.group(1))  # Convert the matched string to integer
+        print(f"File: {filename}, ID: {model3_id}")
+        model3_dict[model3_id] = filename
+
+sorted_keys = sorted(model3_dict.keys())
+model3_dict_names = {key: model3_dict[key] for key in sorted_keys}
 
 # Create a new dictionary with sorted keys
 
+
+
+#ground truth : RCW annotations
 dict2 = {}
 for filename in os.listdir(true_dir):
     if len(filename)==16:
@@ -37,6 +81,10 @@ for filename in os.listdir(true_dir):
 
 sorted_keys2 = sorted(dict2.keys())
 true_dict = {key: dict2[key] for key in sorted_keys2}
+
+
+
+
 
 master_dict = {}
 for key, value in true_dict.items():
